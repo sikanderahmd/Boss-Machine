60619e2ef96f94e72b6ebf4ead07b935e636d27f
Sikander Ahmed <98826085+skndrahmd@users.noreply.github.com>
2023-02-09 10:05:21 +0500

    add get route for minion id

--- server/api.js
+++ server/api.js
@@ -14,8 +14,31 @@ apiRouter.get('/api/minions', (req, res, next) => {
     res.send(allMinions)
 })
 
-apiRouter.get('/good', (req, res, next) => {
-    res.send('good')
+apiRouter.get('/api/minions/:minionId', (req, res, next) => {
+        const minionId = req.params.minionId;
+        const allMinions = (getAllFromDatabase('minions'))
+        const idInArray = allMinions.some(minion => {
+            return minion.id === minionId
+        })
+
+        if(typeof Number(minionId) === 'number' && idInArray){
+            
+            //console.log(getFromDatabaseById('minions', minionId))
+            res.send(getFromDatabaseById('minions', minionId))
+    
+        }
+        else {
+            console.log("ID is not a number or minion does not exist")
+            res.status(404).send()
+        }
+
+})
+
+apiRouter.post('/api/minions', (req, res, next) => {
+    const newMinion = req.query
+    if(newMinion){
+        const num = 0
+    }
 })
 
 
